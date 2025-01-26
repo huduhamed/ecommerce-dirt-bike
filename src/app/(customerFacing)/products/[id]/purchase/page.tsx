6a1df1d744fabe8c6de86ae3565b75c66dbd6c52
Hudu <ahmedhudu21@gmail.com>
2025-01-26 18:51:10 +0100

    add stripe payment

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,5 +1,10 @@
 import db from '@/db/db';
 import { notFound } from 'next/navigation';
+import Stripe from 'stripe';
+import { CheckoutForm } from './_components/CheckoutForm';
+
+// create stripe instance
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export default async function PurchasePage({ params: { id } }: { params: { id: string } }) {
 	// fetch product
@@ -7,5 +12,20 @@ export default async function PurchasePage({ params: { id } }: { params: { id: s
 
 	// if product does not exist, return notfound.
 	if (product == null) return notFound();
-	return <h1>Hell yh</h1>;
+
+	// create payment intent
+	const paymentIntent = await stripe.paymentIntents.create({
+		amount: product.priceInCents,
+		currency: 'EUR',
+		metadata: {
+			productId: product.id,
+		},
+	});
+
+	// if payment intent is not created, throw error
+	if (paymentIntent.client_secret == null) {
+		throw Error('Stripe failed to create payment');
+	}
+
+	return <CheckoutForm product={product} clientSecret={paymentIntent.client_secret} />;
 }
